707c6b707b6b84958be0ef188113e0953d54eb8d
LeoSirius <is.li.xiaoyu@qq.com>
2020-08-03 16:20:07 +0800

    login and logout

--- words_memo/urls.py
+++ words_memo/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from words_memo.views import home_view, profile_view
+from words_memo.views import home_view, profile_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('profile/', profile_view),
+
+    path('accounts/login/', login_view, name='login'),
+    path('accounts/logout/', logout_view, name='logout'),
+
 ]
--- words_memo/views.py
+++ words_memo/views.py
@@ -1,9 +1,44 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.response import TemplateResponse
+from django.shortcuts import redirect
+
+
 
 def home_view(request):
-    return HttpResponse('home view')
+    return TemplateResponse(request, 'home_page.html')
+
 
 @login_required
 def profile_view(request):
     return HttpResponse('my profile')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        print('request.POST = {}'.format(request.POST))
+        # login_form = LoginForm(request.POST)
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+
+        user = authenticate(username=email, password=password)
+        print('user = {}'.format(user))
+        if not user:
+            return HttpResponse('err not user')
+
+        login(request, user)
+        return redirect('/')
+
+    else:
+        # login_form = LoginForm()
+        pass
+
+    return TemplateResponse(request, 'login_page.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+
+    return redirect('/')
